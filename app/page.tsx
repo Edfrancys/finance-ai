c1ec82f7665f6d33c32268bd93a36beddd39d400
Edfrancys Azevedo <edfrancys.victor@outlook.com>
2024-11-05 21:36:27 +0000

    chore: add githooks

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "./_components/ui/button";
 
 const Home = () => {
-    return (
-        <div className="flex w-screen items-center justify-center">
-            <Button> Home </Button>
-        </div>
-    );
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <Button> Home </Button>
+    </div>
+  );
 };
 
 export default Home;
